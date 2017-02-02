22f96fda40a05735c86d8ba1c2a175d7bc73568c
santiruizt <sruiztoribio@gmail.com>
2017-02-03 00:03:50 +0100

    Change navbar on scroll

--- js/navbar.js
+++ js/navbar.js
@@ -1,25 +1,36 @@
-// get the value of the bottom of the #main element by adding the offset of that element plus its height, set it as a variable
-var mainbottom = $('#main').offset().top + $('#main').height();
+// // get the value of the bottom of the #main element by adding the offset of that element plus its height, set it as a variable
+// var mainbottom = $('#main').offset().top + $('#main').height();
 
-// on scroll, 
-$(window).on('scroll',function(e){
+// // on scroll, 
+// $(window).on('scroll',function(e){
 
-    // we round here to reduce a little workload
-    var stop = Math.round($(window).scrollTop());
-    var sectionA = document.querySelector('.content-section-a');
+//     // we round here to reduce a little workload
+//     var stop = Math.round($(window).scrollTop());
+//     var sectionA = document.querySelector('.content-section-a');
 
     
 
-    // if (stop > mainbottom) {
-    //     $('.navbar-default').addClass('past-main');
-    // } else {
-    //     $('.navbar-default').removeClass('past-main');
-    // }
+//     // if (stop > mainbottom) {
+//     //     $('.navbar-default').addClass('past-main');
+//     // } else {
+//     //     $('.navbar-default').removeClass('past-main');
+//     // }
 
-    if (sectionA.offsetTop < window.scrollY) {
-    	console.log('sectionA.offsetTop > window.scrollY');
-        $('.navbar-default').addClass('past-main');
-    } else {
-        $('.navbar-default').removeClass('past-main');
-    }
+//     if (sectionA.offsetTop < window.scrollY) {
+//     	console.log('sectionA.offsetTop > window.scrollY');
+//         $('.navbar-default').addClass('past-main');
+//     } else {
+//         $('.navbar-default').removeClass('past-main');
+//     }
+// });
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
 });
+
+
+
+
